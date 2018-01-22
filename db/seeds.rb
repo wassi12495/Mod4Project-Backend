4a20d51eb5abf9ac575e76453d3b67052c77d679
erickalexander <erickcamacho1@gmail.com>
2018-01-22 14:36:44 -0500

    fixed imgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,7 @@ require 'football__data'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 5fc2c9d1d19e2e1fbe338a33242cd1cafe4db17e
 FootballData.configure do |config|
   config.api_key = "4a9ace89a355471280f93cb35479661b"
   config.api_version = 'v1'
@@ -22,11 +19,7 @@ end
 
 
 league_resp = FootballData.fetch(:competitions)
-<<<<<<< HEAD
-# league_json = JSON.parse(league_resp)
-=======
 
->>>>>>> 5fc2c9d1d19e2e1fbe338a33242cd1cafe4db17e
 
 league_resp.each do |league|
   if league["id"] == 445 || league["id"] ==449 || league["id"] ==450 || league["id"] ==452 || league["id"] ==455 ||league["id"] ==456
@@ -34,10 +27,6 @@ league_resp.each do |league|
   end
 end
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5fc2c9d1d19e2e1fbe338a33242cd1cafe4db17e
 League.all.each do |league|
   teams_json = FootballData.fetch(:competitions, :leagueTable, id:league.api_id)
   if !teams_json["error"]
@@ -48,32 +37,27 @@ League.all.each do |league|
     league.teams = league_teams
   end
 end
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 5fc2c9d1d19e2e1fbe338a33242cd1cafe4db17e
 
 
 
-# league_logos = [{:league_abr => "BSA", :img => "https://upload.wikimedia.org/wikipedia/en/4/42/Campeonato_Brasileiro_S%C3%A9rie_A_logo.png"},
-# {:league_abr => "PL", :img  => "https://goo.gl/sRzbsy"},
-# {:league_abr => "ELC", :img => "https://goo.gl/iZywxV"},
-# {:league_abr => "EL1", :img => "https://goo.gl/h5Uafj"},
-# {:league_abr => "EL2", :img => "https://goo.gl/7qEhcH"},
-# {:league_abr => "DED", :img => "https://goo.gl/RouvRN"},
-# {:league_abr => "FL1", :img => "https://goo.gl/jcGJez"},
-# {:league_abr => "FL2", :img => "https://goo.gl/fp5pfo"},
-# {:league_abr => "BL1", :img => "https://goo.gl/jBgj8w"},
-# {:league_abr => "BL2", :img => "https://goo.gl/TjKvCA"},
-# {:league_abr => "PD", :img => "https://goo.gl/1p1saH"},
-# {:league_abr => "SA", :img => "https://goo.gl/xCdm53"},
-# {:league_abr => "PPL", :img => "https://goo.gl/aXhyKG"},
-# {:league_abr => "DFB", :img => "https://goo.gl/8nxhyj"},
-# {:league_abr => "SB", :img => "https://goo.gl/CQ8TtL"},
-# {:league_abr => "CL", :img => "https://goo.gl/S91KE9"},
-# {:league_abr => "AAL", :img => "https://goo.gl/fwyVxL"}]
-#
-# league_logos.each do |key, value|
-#   puts key
-# end
+league_logos = [{:league_abr => "BSA", :img => "https://goo.gl/ErU9RJ"},
+{:league_abr => "PL", :img  => "https://goo.gl/sRzbsy"},
+{:league_abr => "ELC", :img => "https://goo.gl/iZywxV"},
+{:league_abr => "EL1", :img => "https://goo.gl/h5Uafj"},
+{:league_abr => "EL2", :img => "https://goo.gl/7qEhcH"},
+{:league_abr => "DED", :img => "https://goo.gl/RouvRN"},
+{:league_abr => "FL1", :img => "https://goo.gl/jcGJez"},
+{:league_abr => "FL2", :img => "https://goo.gl/fp5pfo"},
+{:league_abr => "BL1", :img => "https://goo.gl/jBgj8w"},
+{:league_abr => "BL2", :img => "https://goo.gl/TjKvCA"},
+{:league_abr => "PD", :img => "https://goo.gl/1p1saH"},
+{:league_abr => "SA", :img => "https://goo.gl/xCdm53"},
+{:league_abr => "PPL", :img => "https://goo.gl/aXhyKG"},
+{:league_abr => "DFB", :img => "https://goo.gl/8nxhyj"},
+{:league_abr => "SB", :img => "https://goo.gl/CQ8TtL"},
+{:league_abr => "CL", :img => "https://goo.gl/S91KE9"},
+{:league_abr => "AAL", :img => "https://goo.gl/fwyVxL"}]
+
+league_logos.each do |key, value|
+  puts key
+end
