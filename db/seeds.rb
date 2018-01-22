1fc7eb65a13f3eca7277aa0a5db565ad28725452
erickalexander <erickcamacho1@gmail.com>
2018-01-22 14:55:10 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,24 +40,15 @@ end
 
 
 
-league_logos = [{:league_abr => "BSA", :img => "https://goo.gl/ErU9RJ"},
-{:league_abr => "PL", :img  => "https://goo.gl/sRzbsy"},
-{:league_abr => "ELC", :img => "https://goo.gl/iZywxV"},
-{:league_abr => "EL1", :img => "https://goo.gl/h5Uafj"},
-{:league_abr => "EL2", :img => "https://goo.gl/7qEhcH"},
+league_logos = [{:league_abr => "PL", :img  => "https://goo.gl/sRzbsy"},
 {:league_abr => "DED", :img => "https://goo.gl/RouvRN"},
 {:league_abr => "FL1", :img => "https://goo.gl/jcGJez"},
-{:league_abr => "FL2", :img => "https://goo.gl/fp5pfo"},
 {:league_abr => "BL1", :img => "https://goo.gl/jBgj8w"},
-{:league_abr => "BL2", :img => "https://goo.gl/TjKvCA"},
 {:league_abr => "PD", :img => "https://goo.gl/1p1saH"},
-{:league_abr => "SA", :img => "https://goo.gl/xCdm53"},
-{:league_abr => "PPL", :img => "https://goo.gl/aXhyKG"},
-{:league_abr => "DFB", :img => "https://goo.gl/8nxhyj"},
-{:league_abr => "SB", :img => "https://goo.gl/CQ8TtL"},
-{:league_abr => "CL", :img => "https://goo.gl/S91KE9"},
-{:league_abr => "AAL", :img => "https://goo.gl/fwyVxL"}]
+{:league_abr => "SA", :img => "https://goo.gl/xCdm53"}]
 
-league_logos.each do |key, value|
-  puts key
+league_logos.each do |hash|
+  league = League.find_by(:league_abr == hash[:league_abr])
+  league.img = hash[:img]
+  league.save
 end
