ee03e828409d78f832e41da0c8fcf26e13c9985a
github username <github email address>
2018-01-23 14:11:42 -0500

    added bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,8 @@ ActiveRecord::Schema.define(version: 20180118201040) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
