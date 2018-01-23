d2d41fddd0a19013b62a191ac0b15847f820fd6d
github username <github email address>
2018-01-23 14:49:54 -0500

    Beginning of auth implementation, Modified User controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
-class api::v1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+
+  def create
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      render json: {username: user.username, id: user.id}
+    else
+      render json: {error: 'User information is not authenticated'}, status: 401
+    end
+  end
+
+
 end
