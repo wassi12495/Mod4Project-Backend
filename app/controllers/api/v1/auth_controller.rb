a93f893b9148fa83819f6f07d24583978da7f517
github username <github email address>
2018-01-24 10:36:30 -0500

    removed byebug

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::AuthController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    byebug
     if user && user.authenticate(params[:password])
       render json: {username: user.username, id: user.id}
     else
